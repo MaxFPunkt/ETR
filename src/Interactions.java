82bfa9197df0940175e39488f83354c388e8e257
Marcel Cossijns <marcel@cossijns.de>
2017-01-23 09:09:18 +0100

    add Interactions add .gitignore add Main
    Signed-off-by: Marcel Cossijns <marcel@cossijns.de>

---
+++ src/Interactions.java
@@ -0,0 +1,26 @@
+
+public interface Interactions {
+
+	/**
+	 * 
+	 * @return true if the Object can be pushed.
+	 */
+	public default boolean canPush(){return false;}
+	
+	/**
+	 * 
+	 * @return true if the Object can be grabbed.
+	 */
+	public default boolean canGrab(){return false;}
+	
+	/**
+	 * 
+	 * @return true if the Object can be looked at.
+	 */
+	public default boolean canLook(){return false;}
+	
+	public default void push(){}
+	public default void grab(){}
+	public default void look(){}
+	
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class Main extends Application{
+	
+	public static void main(String[] args) {
+		 launch();
+	}
+
+	@Override
+	public void start(Stage stage){
+		stage.show();
+	}
+	
+}
