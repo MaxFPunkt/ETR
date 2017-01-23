cbdeecbdc083bd51b63c18a6e1e30e8c9b1fdaf8
Marcel Cossijns <marcel@cossijns.de>
2017-01-23 10:59:32 +0100

    add Comments to Object
    Signed-off-by: Marcel Cossijns <marcel@cossijns.de>

--- src/objects/Object.java
+++ src/objects/Object.java
@@ -11,17 +11,36 @@ public class Object implements Interactions,Drawable {
 	
 	private double width;
 	private double height;
-
+	
+	/**
+	 * 
+	 * @return the x Position 
+	 */
 	public double getX() {return x;}
+	/**
+	 * 
+	 * @return the y Position 
+	 */
 	public double getY() {return y;}
+	/**
+	 * 
+	 * @return the z Position 
+	 */
 	public double getZ() {return z;}
+	/**
+	 * 
+	 * @return the width of object 
+	 */
 	public double getWidth() {return width;}
+	/**
+	 * 
+	 * @return the height of object 
+	 */
 	public double getHeight() {	return height;}
 	
 	@Override
 	public void draw() {
 		// TODO Auto-generated method stub		
 	}
-
 
 }
