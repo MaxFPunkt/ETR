d3f7c81e5f1c6fa282f7627545a4fa22a420c2d5
Marcel Cossijns <marcel@cossijns.de>
2017-01-23 11:11:28 +0100

    Level implements Timer implement update(double pastTime) in Level
    Signed-off-by: Marcel Cossijns <marcel@cossijns.de>

--- src/Controll/Level.java
+++ src/Controll/Level.java
@@ -5,9 +5,10 @@ import java.util.Comparator;
 import java.util.List;
 
 import objects.interfaces.Drawable;
+import objects.interfaces.Timer;
 import objects.Object;
 
-public class Level implements Drawable{
+public class Level implements Drawable,Timer{
 	private List<Object>  objects= new ArrayList<>();
 	
 	@Override
@@ -16,5 +17,10 @@ public class Level implements Drawable{
 			.sorted(Comparator.comparing(Object::getZ))
 			.forEachOrdered(o->o.draw());
 		
+	}
+	@Override
+	public void update(double pastTime) {
+		objects.stream()
+			.forEach(o->o.call(pastTime));
 	}
 }
