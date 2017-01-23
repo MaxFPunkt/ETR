8b720021465e0f4559bdc4147685d22448150c73
Marcel Cossijns <marcel@cossijns.de>
2017-01-23 14:13:41 +0100

    add new Class Interface

---
+++ src/objects/Interface.java
@@ -0,0 +1,14 @@
+package objects;
+
+import javafx.scene.canvas.GraphicsContext;
+import objects.interfaces.Drawable;
+import objects.interfaces.Timer;
+
+public class Interface implements Drawable,Timer{
+
+	@Override
+	public void draw(GraphicsContext gc, double windowWidth, double windowHeight, double xOffsetWindow) {
+		// TODO Auto-generated method stub		
+	}
+
+}
