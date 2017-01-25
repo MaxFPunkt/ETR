745f08e0be57e5663590cda31aa2ac84a348ba92
Fislball <maxfeser98@web.de>
2017-01-25 14:57:10 +0100

    JavaFX-Interface added Signed-off-by: Fislball <maxfeser98@web.de>

--- src/objects/Interface.java
+++ src/objects/Interface.java
@@ -1,14 +1,26 @@
 package objects;
 
-import javafx.scene.canvas.GraphicsContext;
-import objects.interfaces.Drawable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import objects.interfaces.Timer;
 
-public class Interface implements Drawable,Timer{
-
-	@Override
-	public void draw(GraphicsContext gc, double windowWidth, double windowHeight, double xOffsetWindow) {
-		// TODO Auto-generated method stub		
+public class Interface extends Pane implements Timer{
+	
+	private List<Node> elements;
+	
+	Button toggleAction;
+	
+	public Interface() {
+		elements = new ArrayList<Node>();
+		
+		toggleAction = new Button("Ausfahren");
+		toggleAction.layoutYProperty().bind(this.heightProperty().subtract(toggleAction.heightProperty()));
+		getChildren().add(toggleAction);
 	}
+	
 	public void mouseClicked(double x, double y){}
 }
--- src/view/Content.java
+++ src/view/Content.java
@@ -5,44 +5,27 @@ import java.util.List;
 
 import Controll.Level;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.paint.Color;
-import objects.Interface;
 import objects.interfaces.Timer;
 
 public class Content extends Canvas implements Timer{
 	private List<Level> levels;
 	private double xOffset;
-	private Interface intface;
-	int i=0;
+	
 	public Content(){
 		super(100,100);
 		levels = new ArrayList<Level>();
 		levels.add(new Level(widthProperty(),heightProperty(),1000));
-		intface=new Interface();
 	}
 
 	public void draw() {
-		GraphicsContext gc = getGraphicsContext2D();
-		
-		// Bild reseten
-		gc.clearRect(0, 0, getWidth(), getHeight());
-		gc.setFill(new Color(0.5,0.5,0.5, 1));
-		gc.fillRect(0, 0, getWidth(), getHeight());
-		
-		if(i%30==0){
-			gc.setFill(new Color(Math.random(),Math.random(), Math.random(), 1));
-			i=0;
-		}
-		for(Level l:levels) l.draw(gc,getWidth(),getHeight(),xOffset);
-		i++;
-		intface.draw(gc,  getWidth(),  getHeight(), 0);		
+		getGraphicsContext2D().clearRect(0, 0, getWidth(), getHeight());
+		for(Level l:levels) 
+			l.draw(getGraphicsContext2D(),getWidth(),getHeight(),xOffset);
 	}
 	public void mouseClick(double x, double y) {
 		for(Level l:levels)l.mouseClicked(x,y);
-		intface.mouseClicked(x,y);
 	}
 
 	public void keyEvent(KeyEvent e) {
