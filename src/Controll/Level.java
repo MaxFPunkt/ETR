b445b1edeec70c567fbc467958347542347c6860
Marcel Cossijns <marcel@cossijns.de>
2017-01-30 11:18:07 +0100

    fix Bug
    Signed-off-by: Marcel Cossijns <marcel@cossijns.de>

--- src/Controll/Level.java
+++ src/Controll/Level.java
@@ -31,7 +31,7 @@ public class Level implements Drawable,Timer{
 		backgrounbdWith.bind(height.divide(9).multiply(32));
 		Object vase = new Object(0, 0, 0, 100, 100,new Image("001.png"));
 		vase.setLookText("Hm die Vase sieht sehr leicht aus.");
-		vase.setGrabText("Haha die gehört jetzt mir!");
+		vase.setGrabText("Haha die gehÃ¶rt jetzt mir!");
 		vase.setCanGrab(true);
 		objects.add(vase);
 		objects.add(new Object(120, 50, 0, 100, 100,new Image("001.png")));
@@ -63,7 +63,6 @@ public class Level implements Drawable,Timer{
 			Optional<Object> object=getClickedObjekt(x, y);
 			if(object.isPresent()){
 				Action action = parent.getIntface().action(object.get());
-				System.out.println("Action: "+action.toString());
 				if(action!=null && action.equals(Action.GRAB)) {
 					partikelEffekts.add(new PartikelEffekt(object.get(),lastWindowHeight, lastXOffsetWindow));
 					objects.remove(object.get());
