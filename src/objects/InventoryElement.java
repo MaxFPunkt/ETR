857bbd7abbf2e60b88d27345a35d7eb67be5d14a
Marcel Cossijns <marcel@cossijns.de>
2017-01-30 13:41:30 +0100

    InventoryElement add aktive DropShadow
    Signed-off-by: Marcel Cossijns <marcel@cossijns.de>

--- src/objects/InventoryElement.java
+++ src/objects/InventoryElement.java
@@ -1,11 +1,19 @@
 package objects;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
 public class InventoryElement extends Pane {
 
+	private static ObjectProperty<Object> aktive=new SimpleObjectProperty<>(null);
 	Object object;
+    private static final DropShadow ds = new DropShadow( 20, Color.RED );
+    
 	public InventoryElement(Object object) {
 		this.object=object;
 		setStyle("-fx-effect: dropshadow(gaussian, rgba(0, 0, 0, 0.4), 15, 0.5, 0.0, 0.0);" +
@@ -16,9 +24,20 @@ public class InventoryElement extends Pane {
 			getChildren().add(imageView);
 			imageView.fitWidthProperty().bind(widthProperty());
 			imageView.fitHeightProperty().bind(heightProperty());
+			
+			aktive.addListener((ObservableValue<? extends Object> observable, Object oldValue, Object newValue)->{
+				if(newValue==object){
+					imageView.setEffect( ds );
+				}else{
+					imageView.setEffect( null );
+				}
+			});
 		}
 		setOnMouseClicked(e->{
-			System.out.println("click");
+			if(aktive.get()==object){
+				aktive.set(null);
+			}else
+				aktive.set(object);
 		});
 	}
 	
