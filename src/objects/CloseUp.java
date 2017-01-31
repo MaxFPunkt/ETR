aced37ebd1572c41f715b4ff9c6b0caecb509810
Marcel Cossijns <marcel@cossijns.de>
2017-01-31 08:36:28 +0100

    use in CloseUp now Runnable instance of Consumer<Boolean>
    Signed-off-by: Marcel Cossijns <marcel@cossijns.de>

--- src/objects/CloseUp.java
+++ src/objects/CloseUp.java
@@ -1,7 +1,5 @@
 package objects;
 
-import java.util.function.Consumer;
-
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
@@ -12,7 +10,7 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
 public class CloseUp extends Pane {
-	protected Consumer<Boolean> function;
+	protected Runnable function;
 	protected Pane content=new Pane();
 	public CloseUp(String name) {
 		content.layoutYProperty().bind(heightProperty().divide(2).subtract(250));
@@ -29,11 +27,11 @@ public class CloseUp extends Pane {
 		b.layoutYProperty().bind(heightProperty().divide(2).subtract(330));
 		b.layoutXProperty().bind(widthProperty().divide(2).add(250));
 		b.setOnAction(e->{
-			if(function!=null)function.accept(true);
+			if(function!=null)function.run();
 		});
 		
 	}
-	public void setOnClose(Consumer<Boolean> function){
+	public void setOnClose(Runnable function){
 		this.function=function;
 	}
 }
