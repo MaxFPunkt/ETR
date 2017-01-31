c38a09c4f90945d900ab78c6fc19330b40e8b767
Marcel Cossijns <marcel@cossijns.de>
2017-01-31 09:00:19 +0100

    add Example CodePanel
    Signed-off-by: Marcel Cossijns <marcel@cossijns.de>

--- src/view/Main.java
+++ src/view/Main.java
@@ -10,6 +10,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import objects.CodePanel;
 import objects.Interface;
 import objects.Menu;
 
@@ -70,6 +71,19 @@ public class Main extends Application{
 		menu.prefWidthProperty().bind(mainScene.widthProperty());
 		menu.prefHeightProperty().bind(mainScene.heightProperty());
 		mainPane.getChildren().add(menu);
+		
+		CodePanel codePanel=new CodePanel("123");
+		codePanel.prefWidthProperty().bind(mainScene.widthProperty());
+		codePanel.prefHeightProperty().bind(mainScene.heightProperty());
+		codePanel.setOnClose(()->{
+			mainPane.getChildren().remove(codePanel);
+		});
+		codePanel.setOnEnter(i->{
+			if(i==true){
+				mainPane.getChildren().remove(codePanel);
+			}
+		});
+		//mainPane.getChildren().add(codePanel);
 		
 		stage.addEventHandler(MouseEvent.MOUSE_CLICKED, ( e) -> {	
 			content.mouseClick(e.getSceneX(), e.getSceneY());
